1298f2ae8c3854896a225f557b3d077250f2a1d2
nevesmarcos42 <nevesmarcos42@gmail.com>
2025-07-07 17:18:21 -0300

    feat: finalização de pedido com cupom e produtos comprados

---
+++ src/main/java/com/example/price_wise_fullstack/model/OrderItem.java
@@ -0,0 +1,28 @@
+package com.example.price_wise_fullstack.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "order_items")
+@Data
+public class OrderItem {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Order order;
+
+    @ManyToOne
+    private Product product;
+
+    private BigDecimal price;
+
+}
