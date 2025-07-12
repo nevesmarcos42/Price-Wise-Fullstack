92dd70d493fad13bf726e4f29b2dccb5c4d7d7fb
nevesmarcos42 <nevesmarcos42@gmail.com>
2025-07-12 14:56:53 -0300

    feat: implementa OrderController para gerenciamento de pedidos

--- backend/src/main/java/com/example/price_wise_fullstack/controller/OrderController.java
+++ backend/src/main/java/com/example/price_wise_fullstack/controller/OrderController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrderController {
